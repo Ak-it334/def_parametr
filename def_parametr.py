99f804d0ca4bb515cb150d984c12d00976cbbb02
Ak-it334 <aleksejkudravcev986@gmail.com>
2024-10-07 17:28:17 +0300

    Update def_parametr.py

--- def_parametr.py
+++ def_parametr.py
@@ -1,8 +1,10 @@
-def single_root_words(root_word, *other_words):
+def single_root_words(root_word,*other_words):
     same_words = []
-    for word in other_words:
-        if root_word.startswith(word) or word.startswith(root_word):
-            same_words.append(word)
+    root_word_lower = root_word.lower()
+    for i in other_words:
+        other_words_lower = i.lower()
+        if root_word_lower in other_words_lower or other_words_lower in root_word_lower:
+            same_words.append(i)
     return same_words
 
 
